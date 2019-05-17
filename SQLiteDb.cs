33a3fde5f8e01e6eb19e60ee0eb1b0dd70bd8672
cimpeanuionut <43180388+cimpeanuionut@users.noreply.github.com>
2019-05-17 16:14:47 +0300

    SqLIte for Android Configuration

---
+++ SQLiteDb.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using SQLite;
+using Xamarin.Forms;
+using ProiectMobile.Droid;
+
+[assembly: Dependency(typeof(SQLiteDb))]
+
+namespace ProiectMobile.Droid
+{
+    public class SQLiteDb : ISQLiteDb
+    {
+        public SQLiteConnection DbConnection()
+        {
+            var documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            var path = Path.Combine(documentsPath, "UserDb.db3");
+
+            return new SQLiteConnection(path);
+        }
+    }
+}
